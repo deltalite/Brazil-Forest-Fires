d0303c83b6d382af26a770e8d4ac1d41391fd08d
deltalite <dmvanlee@edu.uwaterloo.ca>
2019-12-28 15:12:03 -0500

    Minor wording edits

--- forest_fire_app/app.R
+++ forest_fire_app/app.R
@@ -3,10 +3,8 @@ library(dplyr)
 library(ggplot2)
 # library(ggrepel)
 library(brazilmaps)
-# library(ggmap)
 # library(tidyverse)
 
-# Enable Google Maps API
 
 # Constants
 PLOT_TYPES <- c('bar', 'cloropleth', 'line', 'scatter')
@@ -14,17 +12,13 @@ names(PLOT_TYPES) <- c('Bar', 'Cloropleth', 'Line', 'Scatter')
 MONTHS <- c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
 SELECT_YEAR <- "Select a year: "
 
-BR_fire_data <- read.csv('../amazon.csv')
+BR_fire_data <- read.csv('../data/amazon.csv')
 BR_fire_data$state <- as.factor(toupper(BR_fire_data$state))
 map_states <- get_brmap(geo = "State", class = "sf")
-# levels(BR_fire_data$state) <- iconv(levels(BR_fire_data$state),from="UTF-8",to="ASCII//TRANSLIT")
-levels(map_states$nome) <- iconv(levels(map_states$nome),from="UTF-8",to="ASCII//TRANSLIT")
 plot_brmap(map_states,
            data_to_join = BR_fire_data,
            join_by = c('nome'='state'),
            var = 'Forest Fires 2018')
-
-map_states <- get_brmap(geo = "State", class = "sf")
 
 ui <- fluidPage(
   titlePanel("Brazil Forest Fires"),
